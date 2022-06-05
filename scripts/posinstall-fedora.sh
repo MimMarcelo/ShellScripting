13cab07be7307479ef27419a3a621ed7818ab653
Marcelo Júnior <rokermarcelo@gmail.com>
2022-06-05 16:51:58 -0300

    Corrigido bug da criação dos links simbólicos e de configuração do Git

--- scripts/posinstall-fedora.sh
+++ scripts/posinstall-fedora.sh
@@ -11,28 +11,31 @@
 # - Adicionar opções como: --version, -dualboot=true/false
 # - Verificação de status das instalações não está funcionando
 # - Revisar comentários
+# - Criar função que executa comandos como usuário local $USER 
 
 ###############################################################################
 # Define as variáveis
 
 spin='-\|/' # Utilizado na função [loading], representa o carregamento
 
+GIT_EMAIL="rokermarcelo@gmail.com"   # e-mail global do Git
+GIT_USER="Marcelo Júnior"            # Usuário global do Git
+USER='marcelo'
 # status=0
 
 ###############################################################################
 # Define as listas
 
 DNF_PROGRAMS=(
-    fedora-workstation-repositories
     gnome-tweak-tool extension
     software-properties-common
-    breeze-gtk breeze-icon-theme breeze-cursor-theme
-    php php-curl php-mysql php-sqlite3 phpunit
+    breeze-cursor-theme
+    php php-curl php-mysql php-sqlite3 php-pgsql phpunit
     composer
+    google-chrome 
     https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
-    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
     ffmpeg
+    \*-firmware
 )
 
 FLATPAK_PROGRAMS=(
@@ -92,6 +95,9 @@ function installFlatpak {
 # Pré-requisitos para execução
 echo ""
 echo "************************************************************************"
+echo "************************************************************************"
+echo "************************************************************************"
+echo "************************************************************************"
 echo "* PREREQUISITS"
 
 # Verifica se o script está sendo executado como sudo
@@ -103,19 +109,23 @@ else
    printf "\r- [OK]                                                          \n"
 fi
 
+# Configuração para melhorar a velocidade de resposta do DNF
+echo 'fastestmirror=True' >> /etc/dnf/dnf.conf
+echo 'max_parallel_downloads=10' >> /etc/dnf/dnf.conf
+dnf clean all
+
+#Configuração do RPM-fusion
+installDNF https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+installDNF https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf groupupdate core -y
+
+#Configuração de recursos multimídia
+dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
+dnf groupupdate sound-and-video -y
+
 # Habilita o gerenciador DNF fedora-extras
 echo "Manager fedora-extras"
 status=$(dnf config-manager --set-enabled fedora-extras -y 2> /dev/null) &
-loading $! # Envia o [id do processo] para a função de loading
-
-if [ $status -gt 0 ]; then
-    printf "\r- [FAILURE]                                                    \n"
-else
-    printf "\r- [OK]                                                         \n"
-fi
-
-echo "Manager google-chrome"
-status=$(dnf config-manager --set-enabled google-chrome -y 2> /dev/null) &
 loading $! # Envia o [id do processo] para a função de loading
 
 if [ $status -gt 0 ]; then
@@ -139,25 +149,11 @@ sh -c 'echo -e "[insync]\nname=insync repo\nbaseurl=http://yum.insync.io/fedora/
 printf "\r- [OK]                                                             \n"
 
 echo "Update DNF dependencies"
-status=$(dnf update -y 2> /dev/null) &
-loading $! # Envia o [id do processo] para a função de loading
-
-if [ $status -gt 0 ]; then
-    printf "\r- [FAILURE]                                                    \n"
-else
-    printf "\r- [OK]                                                         \n"
-fi
+dnf update -y
 
 echo "Update Flatpak dependencies"
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-status=$(flatpak update -y 2> /dev/null) &
-loading $! # Envia o [id do processo] para a função de loading
-
-if [ $status -gt 0 ]; then
-    printf "\r- [FAILURE]                                                    \n"
-else
-    printf "\r- [OK]                                                         \n"
-fi
+flatpak update -y
 
 echo ""
 echo "************************************************************************"
@@ -173,60 +169,15 @@ for program in ${FLATPAK_PROGRAMS[@]}; do
     installFlatpak $program
 done
 
-echo ""
 echo "************************************************************************"
-echo "* REPLACE NAUTILUS WITH NEMO"
-xdg-mime default nemo.desktop inode/directory
-
-echo "Remove Nautilus"
-status=$(dnf remove nautilus -y 2> /dev/null) &
-loading $! # Envia o [id do processo] para a função de loading
-
-if [ $status -gt 0 ]; then
-    printf "\r- [FAILURE]                                                    \n"
-else
-    printf "\r- [OK]                                                         \n"
-fi
-
-echo "Remove Nautilus*"
-status=$(dnf remove nautilus* -y 2> /dev/null) &
-loading $! # Envia o [id do processo] para a função de loading
-
-if [ $status -gt 0 ]; then
-    printf "\r- [FAILURE]                                                    \n"
-else
-    printf "\r- [OK]                                                         \n"
-fi
-
-echo ""
-echo "************************************************************************"
-echo "Please, configure Nemo as follows:"
-echo " 1 - Open Alacarte (Main Menu)"
-echo " 2 - Select 'Acessories' > 'New Item'"
-echo " 3 - Set:"
-echo " 3.1 - name as 'Nemo'"
-echo " 3.2 - command as '/usr/bin/nemo'"
-echo " 3.3 - image as '/usr/share/icons/gnome/256x256/places/folder.png'"
-echo ""
-echo "************************************************************************"
-echo "Please, configure Git as follows:"
-echo " 1 - git config --global user.email 'GIT_EMAIL'"
-echo " 2 - git config --global user.name 'GIT_USER'"
+echo "* GIT GLOBAL CONFIGURATION"
+sudo -H -u $USER bash -c "git config --global user.email \"$GIT_EMAIL\""
+sudo -H -u $USER bash -c "git config --global user.name \"$GIT_USER\""
 
 echo "************************************************************************"
-echo "* UPGRADE, CLEAN AND ENDING"
-echo "Ugrade"
-status=$(dnf upgrade -y 2> /dev/null) &
-loading $! # Envia o [id do processo] para a função de loading
-
-if [ $status -gt 0 ]; then
-    printf "\r- [FAILURE]                                                    \n"
-else
-    printf "\r- [OK]                                                         \n"
-fi
-
+echo "* CLEAN AND ENDING"
 echo "Clean"
-status=$(dnf clean packages -y 2> /dev/null) &
+status=$(dnf clean all 2> /dev/null) &
 loading $! # Envia o [id do processo] para a função de loading
 
 if [ $status -gt 0 ]; then
@@ -235,16 +186,29 @@ else
     printf "\r- [OK]                                                         \n"
 fi
 
+echo "************************************************************************"
+echo "* CREATE HOME FOLDERS"
+sudo -H -u $USER bash -c 'mkdir ~/Projects'
+sudo -H -u $USER bash -c 'mkdir ~/Games'
+sudo -H -u $USER bash -c 'mkdir ~/Insync'
+sudo -H -u $USER bash -c 'mkdir ~/Insync/Documents'
+sudo -H -u $USER bash -c 'mkdir ~/Insync/Music'
+sudo -H -u $USER bash -c 'mkdir ~/Insync/Pictures'
+sudo -H -u $USER bash -c 'mkdir ~/Insync/Projects'
+sudo -H -u $USER bash -c 'mkdir ~/Insync/Videos'
+sudo -H -u $USER bash -c 'mkdir ~/Insync/Games'
 echo "************************************************************************"
 echo "* CREATE SYMBOLIC LINKS"
-ln -s ~/Insync/Documents/ Documents/_Documents
-ln -s ~/Insync/Music/ Music/_Music
-ln -s ~/Insync/Pictures/ Pictures/_Pictures
-ln -s ~/Insync/Projects/ Projects/_Projects
-ln -s ~/Insync/Videos/ Videos/_Videos
-ln -s ~/Insync/Games/ Games/_Games
+sudo -H -u $USER bash -c 'ln -s ~/Insync/Documents/ ~/Documents/_Documents'
+sudo -H -u $USER bash -c 'ln -s ~/Insync/Music/ ~/Music/_Music'
+sudo -H -u $USER bash -c 'ln -s ~/Insync/Pictures/ ~/Pictures/_Pictures'
+sudo -H -u $USER bash -c 'ln -s ~/Insync/Projects/ ~/Projects/_Projects'
+sudo -H -u $USER bash -c 'ln -s ~/Insync/Videos/ ~/Videos/_Videos'
+sudo -H -u $USER bash -c 'ln -s ~/Insync/Games/ ~/Games/_Games'
 
 echo "************************************************************************"
 echo "* FORMATE TERMINAL"
 echo PS1=\"\[\e[1;32m\]\u@\h \[\e[37m\]\w $ \[\e[0;37m\]\" >> ~/.bashrc
+echo "Você também pode definir um nome para sua máquina com o comando:"
+echo "hostnamectl set-hostname nome-da-maquina"
 rm 0
